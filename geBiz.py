2ee63d9869d2e7974e5986d46cc32cfb23215bef
ben <wangxuxin1@gmail.com>
2023-08-02 16:41:29 +0800

    gebiz

--- geBiz.py
+++ geBiz.py
@@ -28,42 +28,26 @@ main_window = driver.current_window_handle
 # Define a list to store the extracted data
 companies = []
 
-for page_number in range(1, 3):
+for page_number in range(1, 65):
 
     # We try for each page number, if the page number does not exist, or is there
     # is any other issue, we break the loop and just store the values we have
 
     try:
+        time.sleep(2)
 
         print(f"Page No.{page_number}")
-
-        # if (page_number <= 242):
-        #     time.sleep(5)
-
-        #     print("Before Click")
-        #     page_num_tag.click()
-
-        #     print("Clicked")
-        #     # Wait for the new page to load (you can adjust the sleep duration as needed)
-        #     time.sleep(5)
-
-        #     driver.execute_script("window.scrollTo(0, 0);")
-
-        #     print(f"Companies in page {page_number}: {len(tags)}")
-        #     print("-----------------------------------------\n")
-
-        #     # We want to continue at page 243, so if page number is less than 243, we skip
-        #     continue
-
         driver.execute_script("window.scrollTo(0, 0);")
 
-        tags = tags = driver.find_elements(
+        tags = driver.find_elements(
             By.CLASS_NAME, 'commandLink_TITLE-BLUE')
 
+        tags = [tag.get_attribute("href") for tag in tags]
+
         print(f"Companies in page {page_number}: {len(tags)}")
+        print(tags)
         print("-----------------------------------------\n")
 
-        tags = [tag.get_attribute("href") for tag in tags]
 
         # Loop through each tag and extract the company information
         for tag in tags:
@@ -82,15 +66,18 @@ for page_number in range(1, 3):
                 # Switch to the new page's window handle
                 driver.switch_to.window(driver.window_handles[-1])
 
-                
+                # //*[@id="contentForm:j_idt468:j_id23:j_idt469"]
+                # //*[@id="contentForm:j_idt468:j_id23:j_idt469"]
+
                 info_tag = driver.find_element(
                     By.ID, 'contentForm:j_idt449')
-                    
+                # By.ID, 'contentForm:j_idt451:j_id23:j_idt452')
+
                 try:
                     # Find all the span tags inside info_tag
                     span = info_tag.find_element(By.TAG_NAME, 'span')
 
-                    #Find tag based on style
+                    # Find tag based on style
 
                     email = span.text.strip()
                 except:
@@ -98,41 +85,14 @@ for page_number in range(1, 3):
 
                 try:
                     # Find all the span tags inside info_tag
-                    name = info_tag.find_element(By.CLASS_NAME, 'form2_ROW-TABLE').text.strip()
+                    name = info_tag.find_element(
+                        By.CLASS_NAME, 'form2_ROW-TABLE').text.strip()
 
                 except:
                     name: "N/A"
-                
+
                 print(f"Name: {name}")
                 print(f"Email: {email}")
-                
-               
-                # # Extract the company name from the new page
-                # try:
-                #     company_name = driver.find_element(By.XPATH,
-                #                                 './/*[@id="app"]/div[8]/div/main/div/div/section/div[1]/div/section/div/div/div[1]/header/div/div[2]/h1').text
-
-                #     company_name = re.sub(r"\bpte\.?\b|\bltd\.?\b|\.", "", company_name, flags=re.IGNORECASE).strip()
-
-                # except:
-                #     company_name = "N/A"
-
-                # try:
-                #     # Extract the email from the new page
-                #     email = driver.find_element(By.XPATH,
-                #                                 '//*[@id="general"]/div/div/div/div[2]/div/div[2]/div/ul[1]/li[3]/a').text
-                # except:
-                #     email = "N/A"
-
-                # try:
-                #     # Extract the email from the new page
-                #     phone_num_li = driver.find_element(By.CLASS_NAME,
-                #                                 'phone')
-
-                #     phone_num = phone_num_li.find_element(By.TAG_NAME, 'span').text
-
-                # except:
-                #     phone_num = "N/A"
 
                 # # Store the information in an object
 
@@ -153,8 +113,19 @@ for page_number in range(1, 3):
             except Exception as e:
                 print("Error in tag: ", tag, e)
 
-    except:
-        print("Error in page number: ", page_number)
+        # After accessing all the comapnies in the page, click on the next page button
+        if page_number < 64:
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+            time.sleep(2)
+            
+            next_button = driver.find_element(
+                By.ID, f'contentForm:j_idt831:j_idt882_Next_{page_number + 1}')
+            
+            next_button.click()
+
+    except Exception as e:
+        print("Error in page number: ", page_number, "Error:", e)
 
 
 # # Create a DataFrame from the companies list
